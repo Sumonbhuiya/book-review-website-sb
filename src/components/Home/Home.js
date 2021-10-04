0c0cc2ed925cbdb2dc05c1a0e54b07d55e722f72
Sumonbhuiya <sumonbhuiya97@gmail.com>
2021-10-04 21:01:17 +0600

    home course add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import useServices from '../../hooks/useServices';
+import React, { useEffect, useState } from 'react';
 import HomeCourse from '../HomeCourse/HomeCourse';
 import Plans from '../Plans/Plans';
 
 const Home = () => {
-    const [services] = useServices();
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        // load data 
+        fetch('./homeTools.JSON')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, []);
+
     return (
         <div>
             {/* import plans component  */}
@@ -12,7 +19,7 @@ const Home = () => {
             <div className="p-6 w-4/5 m-auto bg-gray-300">
                 {/* added some courses by using map from data */}
                 <h2 className="text-3xl pb-6 font-thin">For you</h2>
-                <div className="grid grid-cols-3 gap-8">
+                <div className="grid grid-cols-2 gap-8">
                     {
                         services.map(service => <HomeCourse
                             key={service.key}
--- src/components/HomeCourse/HomeCourse.js
+++ src/components/HomeCourse/HomeCourse.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const HomeCourse = (props) => {
-    const { key } = props.service;
+    const { name, pic, teacher, cost, date } = props.service;
     return (
-        // home page courses show 
-        <div>
-            <h2>{key}</h2>
+        // service cart 
+        <div className="p-3 rounded-md border border-gray-200 bg-gray-100">
+            {/* header  */}
+            <div className="h-75 bg-black">
+                <img className="h-50 w-100% m-auto " src={pic} alt="" />
+                <h3 className="text-white m-auto text-2xl">introduction to {name}</h3>
+            </div>
+            {/* mid details  */}
+            <div className=" pt-2 px-5 flex">
+                <div className="text-left">
+                    <h4 className="text-2xl text-blue-600 font-bold">{name}</h4>
+                    <p><small className="text-gray-500 pt-5">Teacher: <NavLink to="/teacher" className="bg-yellow-200">{teacher.name}</NavLink></small></p>
+                    <p><small className="text-gray-500">Start Date: {date}</small></p>
+                </div>
+                <div className="m-auto pl-8">
+                    <p className="text-3xl text-gray-700 hover:text-blue-500">${cost}</p>
+                </div>
+            </div>
+            {/* footer or button  */}
+            <div className="pt-6">
+                <button className="text-base text-white py-2 px-8 rounded-md border border-gray-200 bg-red-400 hover:bg-red-500">Enroll</button>
+            </div>
         </div>
     );
 };
